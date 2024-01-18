851149142795c60b4e7b610ec746dd3e2456678f
yusef02 <youssef.bouziane2402@gmail.com>
2024-01-18 16:58:01 +0100

    bug evento button next

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ console.log("js ok");
 const carouselElement = document.querySelector(".carousel");
 const arrowPrevElement = document.getElementById("arrowPrev");
 const arrowNextElement = document.getElementById("arrowNext");
-
+console.log(arrowNextElement);
 const images = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 
 for (let i = 0; i < images.length; i++) {
@@ -19,19 +19,20 @@ let slideIndex = 0;
 console.log(slidesList[slideIndex]);
 
 arrowNextElement.addEventListener("click", function () {
-  console.log(" hsjhklfhaskhfkjds");
+  console.log("prova");
   let slide = slidesList[slideIndex];
+  console.log(slideIndex);
   slide.classList.remove("d-block");
   slide.classList.add("d-none");
-  console.log(slide);
 
-  // // slideIndex = slideIndex == slidesList.length - 1 ? 0 : slideIndex + 1;
-  // if (slideIndex == slidesList.length - 1) {
-  //   slideIndex = 0;
-  // } else {
-  //   slideIndex++;
-  // }
-  // slide.classList.remove("d-none");
-  // slide.classList.add("d-block");
-  // console.log(slide);
+  // slideIndex = slideIndex == slidesList.length - 1 ? 0 : slideIndex + 1;
+  if (slideIndex == slidesList.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  slide = slidesList[slideIndex];
+  slide.classList.remove("d-none");
+  slide.classList.add("d-block");
+  console.log(slideIndex);
 });
