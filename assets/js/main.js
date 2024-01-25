f4a4125c18cf79b85f3acc9e68d8a437ecfa75eb
yusef02 <youssef.bouziane2402@gmail.com>
2024-01-25 20:11:31 +0100

    feat(script main.js): added function and interval slides autoplay  exercise 22-01-24

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,37 +15,70 @@ const arrowNextElement = document.getElementById("arrowNext");
 
 const slidesList = document.getElementsByClassName("slide");
 let slideIndex = 0;
-console.log(slidesList[slideIndex]);
 
 arrowNextElement.addEventListener("click", function () {
-  let slide = slidesList[slideIndex];
+  slideIndex = goToSlide(this, slidesList, slideIndex);
+  // let slide = slidesList[slideIndex];
 
-  slide.classList.remove("d-block");
-  slide.classList.add("d-none");
+  // slide.classList.remove("d-block");
+  // slide.classList.add("d-none");
 
-  // slideIndex = slideIndex == slidesList.length - 1 ? 0 : slideIndex + 1;
-  if (slideIndex == slidesList.length - 1) {
-    slideIndex = 0;
-  } else {
-    slideIndex++;
-  }
-  slide = slidesList[slideIndex];
-  slide.classList.remove("d-none");
-  slide.classList.add("d-block");
+  // // slideIndex = slideIndex == slidesList.length - 1 ? 0 : slideIndex + 1;
+  // if (slideIndex == slidesList.length - 1) {
+  //   slideIndex = 0;
+  // } else {
+  //   slideIndex++;
+  // }
+  // slide = slidesList[slideIndex];
+  // slide.classList.remove("d-none");
+  // slide.classList.add("d-block");
 });
+
 arrowPrevElement.addEventListener("click", function () {
-  let slide = slidesList[slideIndex];
+  slideIndex = goToSlide(this, slidesList, slideIndex);
+  // let slide = slidesList[slideIndex];
+  // slide.classList.remove("d-block");
+  // slide.classList.add("d-none");
+  // // slideIndex = slideIndex == slidesList.length - 1 ? 0 : slideIndex + 1;
+  // if (slideIndex == 0) {
+  //   slideIndex = slidesList.length - 1;
+  // } else {
+  //   slideIndex--;
+  // }
+  // slide = slidesList[slideIndex];
+  // slide.classList.remove("d-none");
+  // slide.classList.add("d-block");
+});
+
+function goToSlide(arrowElement, slideCollection, indexSlide) {
+  let slide = slideCollection[indexSlide];
 
   slide.classList.remove("d-block");
   slide.classList.add("d-none");
 
-  // slideIndex = slideIndex == slidesList.length - 1 ? 0 : slideIndex + 1;
-  if (slideIndex == 0) {
-    slideIndex = slidesList.length - 1;
-  } else {
-    slideIndex--;
+  if (arrowElement.id == "arrowNext") {
+    indexSlide = indexSlide == slideCollection.length - 1 ? 0 : indexSlide + 1;
   }
-  slide = slidesList[slideIndex];
+  if (arrowElement.id == "arrowPrev") {
+    indexSlide = indexSlide == 0 ? slideCollection.length - 1 : indexSlide - 1;
+  }
+
+  slide = slideCollection[indexSlide];
   slide.classList.remove("d-none");
   slide.classList.add("d-block");
+  return indexSlide;
+}
+
+let slidesAutoplay = setInterval(function () {
+  arrowNextElement.click();
+}, 3000);
+
+const carouselContainer = document.querySelector(".col.carousel");
+carouselContainer.addEventListener("mouseover", function () {
+  clearInterval(slidesAutoplay);
+});
+carouselContainer.addEventListener("mouseleave", function () {
+  slidesAutoplay = setInterval(function () {
+    arrowNextElement.click();
+  }, 3000);
 });
